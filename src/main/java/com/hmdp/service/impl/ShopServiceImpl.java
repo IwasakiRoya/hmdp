42fe07f10dc3ce39279c80de4aab5e52610fa022
ikroya <ikroya1459@gmail.com>
2025-04-15 21:06:04 +0800

    优化：优化了店铺访问时的缓存穿透

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.ssh.JschUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -19,8 +20,7 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -41,17 +41,25 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         String key = CACHE_SHOP_KEY + id;
         // 1.从Redis中查数据（缓存）
         String shopJson = stringRedisTemplate.opsForValue().get(key);
-        // 2.查询是否存在
-        if (shopJson != null) {
+        // 2.查询是否存在，且不为null
+        if (StrUtil.isNotBlank(shopJson)) {
             // 3.如果存在，直接返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        // 如果为空值（非null）
+        if (shopJson != null) {
+            // 直接返回错误
+            return Result.fail("店铺不存在");
+        }
+        // 如果是null，说明Redis里没有数据，需要去数据库查
         // 4.如果不存在，根据id查数据库
         // 该方法由于父类ServiceImpl实现
         Shop shop = getById(id);
         // 5.数据库也没有，返回错误
+        // 缓存穿透优化，如果不存在，需要将空值写入缓存，然后返回false
         if (shop == null) {
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, java.util.concurrent.TimeUnit.MINUTES);
             return Result.fail("店铺不存在");
         }
 
